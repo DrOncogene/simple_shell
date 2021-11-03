f9390c028f1ea97c7fc2be5ac3c9a864673c39ee
Nasir Abdulrasheed <mypythtesting@gmail.com>
2021-11-03 10:34:30 +0100

    task 3 or shell 0.2 files

---
+++ task-3/main.h
@@ -0,0 +1,14 @@
+#include <sys/types.h>
+
+#ifndef MAIN_H
+#define MAIN_H
+ssize_t get_input(char **, char **);
+int check_builtin(char *);
+void print_env(char **);
+void exec_builtin(int, char **);
+void free_av_buff(char *, char **);
+char **create_argv(char *);
+char *get_full_path(char *);
+char *check_path(char *, char *, char **);
+int execute(char **, char **);
+#endif
