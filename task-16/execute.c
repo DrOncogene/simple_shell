cc65d9153bfecd4bb1475b2da84175f2b28e07fd
Nasir Abdulrasheed <mypythtesting@gmail.com>
2021-11-09 17:37:56 +0100

    final task 16 tasks with readme

---
+++ task-16/execute.c
@@ -0,0 +1,38 @@
+#include "shell.h"
+
+/**
+  * execute - executes a program in a child process with the arguments passed
+  * @args: the argument vector
+  * @env: the environment to pass to the program
+  * Return: 0 if success, -1 otherwise
+  */
+int execute(char **args, char **env)
+{
+	pid_t child;
+	int status;
+	struct stat st;
+
+	if (stat(args[0], &st) == -1)
+		return (-1);
+
+	child = fork();
+	if (child == -1)
+	{
+		return (-1);
+	}
+
+	if (child == 0)
+	{
+		if (execve(args[0], args, env) == -1)
+			_exit(-1);
+	}
+	else
+	{
+		if (waitpid(child, &status, 0) == -1)
+			return (-1);
+		if (WIFEXITED(status))
+			errno = WEXITSTATUS(status);
+	}
+
+	return (0);
+}
---
+++ task-16/shell.h
@@ -0,0 +1,34 @@
+#ifndef SHELL_H
+#define SHELL_H
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+extern char **environ;
+ssize_t get_input(char **, char **, char **);
+char *check_separator(char *);
+char *logical_and(char *);
+char *logical_or(char *);
+int check_builtin(char *);
+void print_env(char **);
+void env_func(char **);
+void exec_builtin(int, char **);
+void ch_dir(char**);
+void free_args(char **);
+void free_args_exit(char **);
+char **parse_command(char *);
+char *get_full_path(char *);
+char *find_path(char *, char *, char **);
+int execute(char **, char **);
+int str_cmp(char *s1, char *s2);
+int str_ncmp(char *s1, char *s2, size_t n);
+unsigned long int str_len(char *s);
+char *mem_cpy(char *dest, const char *src, size_t n);
+char *str_dup(char *s);
+#endif
